e7557f119f677b1458558c9a8baf1878f59f474b
Elorm Ofori <Ofori.elorm@gmail.com>
2025-02-24 22:01:28 +0000

    Access Control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,9 +2,12 @@ from typing import Any
 from django.shortcuts import render,redirect
 from .models import Book, Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
+from django.contrib.auth.decorators import permission_required
+
 
 
 # Create your views here.
@@ -46,3 +49,36 @@ class CustomLoginView(LoginView):
 #user Logout View
 class CustomLogoutView(LogoutView):
     template_name = "logout.html"
+
+#Homepage View
+def index(request):
+    return render(request, "index.html")
+
+#Setting Up Role-Based Views
+#Checks if user is Admin
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@login_required
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+#Checks if user is Librarian
+def is_librarian(user):
+    return user.userprofile.role == 'Librarian'
+
+@login_required
+@user_passes_test(is_librarian)
+
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+#Checks if user is a Member
+def is_member(user):
+    return user.userprofile.role == 'Member'
+
+@login_required
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
