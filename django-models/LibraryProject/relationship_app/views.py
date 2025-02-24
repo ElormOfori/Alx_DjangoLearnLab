87e79f03005d313845ff39f1bce8a2979c06e52c
Elhorm she-codes <132413787+ElormOfori@users.noreply.github.com>
2025-02-24 21:28:30 +0000

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from typing import Any
 from django.shortcuts import render
 from .models import Book
 from .models import Library
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 #Function-based views
 def list_books(request):
