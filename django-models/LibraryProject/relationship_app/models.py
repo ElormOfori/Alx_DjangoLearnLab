8411ad57ddb33a1c127de7b07439a5947c93628d
Elorm Ofori <Ofori.elorm@gmail.com>
2025-02-24 22:07:32 +0000

    Role-based access

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 from django.db import models
@@ -30,3 +31,19 @@ class Librarian(models.Model):
     def __str__(self):
         return self.name
 
+#Extending User Model with a UserProfile
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    Role_Choices =(
+        ('Admin','Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
+    
+    )
+
+role = models.CharField(max_length=50,  choices='Role_Choices')
+userprofile = models.TextField()
+
+def __str__(self):
+    return f'{self.user.username} - {self.role}'
