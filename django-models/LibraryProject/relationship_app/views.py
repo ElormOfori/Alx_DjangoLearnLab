0ec6e4796e3dc19c8c5df124e697e7e8c3083030
Elorm Ofori <Ofori.elorm@gmail.com>
2025-02-24 20:54:17 +0000

    corrected view.py file

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,28 +1,12 @@
-
-# Create your views here.
 from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Book, Library
 
-def list_books(request):
-    books = Book.objects.all()  #Ensure this query is present
-    return render(request, "relationship_app/list_books.html", {"books": books})  #Ensure correct template is used
-
-class LibraryDetailView(DetailView):
-    model = Library
-    template_name = "relationship_app/library_detail.html"  #Ensure correct template is used
-    context_object_name = "library"  # Allows template to use {{ library }} instead of {{ object }}
 
 # Create your views here.
-from django.shortcuts import render
-from .models import Book
-
 def list_books(request):
     books = Book.objects.all()  #Ensure this query is present
     return render(request, "relationship_app/list_books.html", {"books": books})  #Ensure correct template is used
-
-from django.views.generic import DetailView
-from .models import Library
 
 class LibraryDetailView(DetailView):
     model = Library
