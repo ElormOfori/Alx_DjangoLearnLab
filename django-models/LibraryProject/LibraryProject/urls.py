543525455d39b03fc22f72218dc7731ed5930432
Elorm Ofori <Ofori.elorm@gmail.com>
2025-03-11 22:01:15 +0000

    deleted BlogApp

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-]
-from django.urls import path
-from .views import list_books, LibraryDetailView
-
-urlpatterns = [
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+   path("", include("relationship_app.urls")),  # Add this line 
 ]
