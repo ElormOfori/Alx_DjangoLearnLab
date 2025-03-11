3e538fe5627845c655ff59ee8f9e91bd91b01494
Elorm Ofori <Ofori.elorm@gmail.com>
2025-03-11 21:55:13 +0000

    advanced_api_project

--- advanced_api_project/api/models.py
+++ advanced_api_project/api/models.py
@@ -6,10 +6,15 @@ from django.db import models
 class Author(models.Model):
     name=models.CharField(max_length= 100 )
 
+    def _str_(self):
+        return self.name
+
 class Book(models.Model):
     title=models.CharField(max_length=200)
     publication_year=models.IntegerField()
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    def _str_(self):
+        return f"{self.title} ({self.publication_year}) by {self.author.name}"
 
 
