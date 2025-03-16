9556a0e7722b15c44d2deeb503581d5a3334b3f8
Elorm Ofori <Ofori.elorm@gmail.com>
2025-03-16 20:29:46 +0000

    Added filtering, searching, and ordering to Book API

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -3,6 +3,8 @@ from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework
+
 
 # List and Retrieve Views - Publicly Accessible (Read-Only)
 class BookListView(generics.ListAPIView):
