f93ce724755ea343a412125b3d2884c97bfc88d5
Elorm Ofori <Ofori.elorm@gmail.com>
2025-03-20 18:43:38 +0000

    blog post management features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -19,6 +19,6 @@ urlpatterns = [
     path('', PostListView.as_view(), name='post-list'),  # Homepage showing all posts
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View post details
     path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create post
-    path('post/edit/<int:pk>/', PostUpdateView.as_view(), name='post-edit'),  # Edit post
-    path('post/delete/<int:pk>/', PostDeleteView.as_view(), name='post-delete'),  # Delete post
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # Edit post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete post
 ]
