8c2efb220ebdb86abdc0f52765d4fe5bb9c7a978
Elorm Ofori <Ofori.elorm@gmail.com>
2025-03-22 09:16:28 +0000

    Search filter fu ctionality

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Tag
 from .models import Comment
+from taggit.forms import TagWidget
 
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -16,7 +17,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'content', 'tag']
-
+        widgets = {
+            'tags': TagWidget(),  
+        }
 
 class CommentForm(forms.ModelForm):
 
